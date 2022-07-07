a77fc8bb4e925634c1c6fd930797c0a99578d4bd
Lataborda <83258732+Lataborda@users.noreply.github.com>
2022-07-07 14:44:49 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import streamlit as st
-import pandas as pd 
-import numpy as np 
 
 
 st.image('data//Logos2.png')
@@ -71,7 +69,7 @@ st.write('* *Solo una parte del almidon contenido en la yuca es extraible. Segun
 st.subheader(round(AlmRe,1))
 
 
-st.write('**NOTA**: Los valores calculados en esta página con la ecuación de CIAT 1976 (Kawano et al. 1987; Keating et al. 1981) son indicativos. Para valores más precisos se recomiendan otros métodos tal como secado al horno (105°C por 24 horas) para la materia seca, y determinacion enzimatica de almidon (Holm et al. 1986) para el almidon. Esta herramienta esta a disposicion sin garantía de ningún tipo, ya sea expresa o implícita, incluyendo, pero sin limitarse a, las garantías implícitas de comerciabilidad o pertinencia para un propósito en particular; tanpoco sin garantía alguna en lo que respecta, entre otros, a cualquier daño, pérdida de información, pérdida de beneficios, responsabilidades y/o lesiones causadas por el uso de esta herramienta.')
+st.write('**NOTA**: Los valores calculados en esta página con la ecuación de CIAT 1976 (Kawano et al. 1987; Keating et al. 1981) son indicativos. Para valores más precisos se recomiendan otros métodos tal como secado al horno (105°C por 24 horas) para la materia seca, y determinación enzimática de almidón (Holm et al. 1986) para el almidón. Esta herramienta esta a disposición sin garantía de ningún tipo, ya sea expresa o implícita, incluyendo, pero sin limitarse a, las garantías implícitas de comercialidad o pertinencia para un propósito en particular; tampoco sin garantía alguna en lo que respecta, entre otros, a cualquier daño, pérdida de información, pérdida de beneficios, responsabilidades y/o lesiones causadas por el uso de esta herramienta.')
 
 st.write('**Referencias:**')
 st.write('-Hansupalak N., Piromkraipak P., Tamthirat P., Manitsorasak A., Sriroth K., Tran T. (2016). Biogas reduces the carbon footprint of cassava starch: A comparative assessment with fuel oil. Journal of Cleaner Production 134 part B, 539-546.') 
